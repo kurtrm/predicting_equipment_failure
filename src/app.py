e017e8b1b336a04c0785ffab57a5131643b6b62d
Kurt Maurer <kurtrm@gmail.com>
2018-05-11 16:18:16 -0700

    Shifted some files around.

--- src/app.py
+++ src/app.py
@@ -76,4 +76,4 @@ with the numbers, similar to the procedure in #4.
 """
 
 if __name__ == '__main__':
-    application.run(host='0.0.0.0', port=8000, debug=True)
+    application.run(host='0.0.0.0', port=8080, debug=True)
