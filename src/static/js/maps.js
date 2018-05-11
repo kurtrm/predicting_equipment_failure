ea8655f17c541a1fd7287cc219dddfbf3ef5b333
Kurt Maurer <kurtrm@gmail.com>
2018-05-11 09:18:09 -0700

    I think the color scheme is acceptable.

--- src/static/js/maps.js
+++ src/static/js/maps.js
@@ -5,21 +5,29 @@ function initMap() {
     center: {lat: 33.746511, lng: -84.388339}, 
     zoom: 9
   });
-  var square = {
+  var green_square = {
     path: 'M -2,-2 2,-2 2,2 -2,2 z',
-    strokeColor: '#006699',
-    strokeOpacity: .3,
-    fillColor: '#006699',
-    fillOpacity: .3,
+    strokeColor: '#00e600',
+    strokeOpacity: .4,
+    fillColor: '#00e600',
+    fillOpacity: .4,
     scale: 1
   };
-  $.getJSON("../static/data/lat_long.json", function(d){
+  var grey_square = {
+    path: 'M -2,-2 2,-2 2,2 -2,2 z',
+    strokeColor: '#999999',
+    strokeOpacity: .6,
+    fillColor: '#999999',
+    fillOpacity: .6,
+    scale: 1
+  };
+  $.getJSON("../static/data/lat_long.json", function(d) {
   for (var i = 0; i < d.length; i++) {
       var marker = new google.maps.Marker({
       position: {lat: d[i]["Latitude"],
                  lng: d[i]["Longitude"]},
       map: map,
-      icon: square,
+      icon: (d[i]["Status"] === 1) ? green_square : grey_square,
       title: 'Hello World!'
       }); 
     }
