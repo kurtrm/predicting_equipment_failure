9920fb2c2a5c60fde6667394f8af955a43e05b8c
Kurt Maurer <kurtrm@gmail.com>
2018-05-16 15:10:54 -0700

    Map showing up, got the grid system to work in my favor to mostly center the map. Still very slow since I'm pumping all the data into it.

--- src/static/js/new_transformer.js
+++ src/static/js/new_transformer.js
@@ -1,53 +1,54 @@
 "use strict";
+(function(window, document) {
+    $(".results").hide()
 
-$(".results").hide()
 
-
-let get_form_inputs = function() {
-    let veg = $("input:checked");
-    let age = $("input#Age").val();
-    var inputs = {}
-    for (let i = 0; i < veg.length; i++) {
-        if (i <= 4) {
-        inputs[veg[i].name] = veg[i].value;
-        } else {        
-        inputs[veg[i].id] = veg[i].value;
+    let get_form_inputs = function() {
+        let veg = $("input:checked");
+        let age = $("input#Age").val();
+        var inputs = {}
+        for (let i = 0; i < veg.length; i++) {
+            if (i <= 4) {
+            inputs[veg[i].name] = veg[i].value;
+            } else {        
+            inputs[veg[i].id] = veg[i].value;
+            };
         };
-    };
-    inputs["Age"] = age;
-    return inputs;
-}
-
-let send_form_inputs = function(inputs) {
-    $.ajax({
-        url: '/transformer_prediction',
-        contentType: "application/json; charset=utf-8",
-        type: 'POST',
-        success: function(data) {
-            display_prediction(data);
-        },
-        data: JSON.stringify(inputs)
-    });
-};
-
-let display_prediction = function(data) {
-    $(".results").show()
-    $("span#probability").html(data.probability)
-    $("span#threshold").html(data.threshold)
-    if (data.probability < data.threshold) {
-        var assess = "There is a " + data.probability + "% probability that this transformer will not incur additional costs." +
-                     " Additionally, based on the selected threshold of " + data.threshold + ", " + 
-                     "it is recommended that an enhanced preventive maintenance schedule be applied to this unit.";
-    } else {
-        var assess = "There is a " + data.probability + "% probability that this transformer will not incur additional costs." +
-                     " Additionally, based on the selected threshold of " + data.threshold + ", " + 
-                     "it is recommended that this unit maintain a normal preventive maintenance schedule.";
+        inputs["Age"] = age;
+        return inputs;
     }
-    $("span#assessment").html(assess)
-};
 
-$("button#send").click(function() {
-    console.log('Here');
-    let inputs = get_form_inputs();
-    send_form_inputs(inputs);
-});
+    let send_form_inputs = function(inputs) {
+        $.ajax({
+            url: '/transformer_prediction',
+            contentType: "application/json; charset=utf-8",
+            type: 'POST',
+            success: function(data) {
+                display_prediction(data);
+            },
+            data: JSON.stringify(inputs)
+        });
+    };
+
+    let display_prediction = function(data) {
+        $(".results").show()
+        $("span#probability").html(data.probability)
+        $("span#threshold").html(data.threshold)
+        if (data.probability < data.threshold) {
+            var assess = "There is a " + data.probability + "% probability that this transformer will not incur additional costs." +
+                         " Additionally, based on the selected threshold of " + data.threshold + ", " + 
+                         "it is recommended that an enhanced preventive maintenance schedule be applied to this unit.";
+        } else {
+            var assess = "There is a " + data.probability + "% probability that this transformer will not incur additional costs." +
+                         " Additionally, based on the selected threshold of " + data.threshold + ", " + 
+                         "it is recommended that this unit maintain a normal preventive maintenance schedule.";
+        }
+        $("span#assessment").html(assess)
+    };
+
+    $("button#send").click(function() {
+        console.log('Here');
+        let inputs = get_form_inputs();
+        send_form_inputs(inputs);
+    });
+})(window, document);
--- src/static/js/profit_curve.js
+++ src/static/js/profit_curve.js
@@ -1,32 +1,13 @@
 "use strict";
-
+(function(window, document) {
 // I leaned heavily on D3 documentation and lots of examples to generate this code.
 
-var svg = d3.select("#profit_curve"),
-    margin = {top: 20, right: 20, bottom: 30, left: 20},
-    width = +svg.attr("width") - 400 - margin.left - margin.right,
-    height = +svg.attr("height") - margin.top - margin.bottom;
+  var svg = d3.select("#profit_curve"),
+      margin = {top: 20, right: 20, bottom: 30, left: 20},
+      width = +svg.attr("width") - 400 - margin.left - margin.right,
+      height = +svg.attr("height") - margin.top - margin.bottom;
 
 
-var x = d3.scaleLinear()
-    .range([0, width]);
-
-var y = d3.scaleLinear()
-    .range([0, height]);
-
-var line = d3.line()
-    .x(d => x(d.threshold))
-    .y(d => y(d.loss));
-
-var g = svg.append("g")
-    .attr("transform", "translate(" + (margin.left + 50) + "," + margin.top + ")");
-
-d3.json("/retrieve_profit_curve", function(thisData) {
-  draw(thisData);
-});
-
-let draw = function(data) {
-  $("svg").empty()
   var x = d3.scaleLinear()
       .range([0, width]);
 
@@ -37,145 +18,165 @@ let draw = function(data) {
       .x(d => x(d.threshold))
       .y(d => y(d.loss));
 
-  var y_max = d3.max(data, d => d.loss);
-  var x_val = data.filter(d => d.loss === y_max)[0].threshold;
-  var horiz = [{"x1": 0.0, "y1": y_max}, {"x1": x_val, "y1": y_max}]
-  var vert = [{"x2": x_val, "y2": d3.min(data, d => d.loss)}, {"x2": x_val, "y2": y_max}]
-
-  var horiz_line = d3.line()
-      .x(d => x(d.x1))
-      .y(d => y(d.y1));
-
-  var vert_line = d3.line()
-      .x(d => x(d.x2))
-      .y(d => y(d.y2));
-
   var g = svg.append("g")
       .attr("transform", "translate(" + (margin.left + 50) + "," + margin.top + ")");
-  
-  x.domain([0, d3.max(data, d => d.threshold)]);
-  y.domain([d3.max(data, d => d.loss), d3.min(data, d => d.loss)]);
 
-  g.append("g")
-      .attr("class", "axis axis--x")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x))
-    .append("text")
-     .attr("class", "axis-title")
-     .attr("y", 18)
-     .attr("dy", "1em")
-     .attr("x", (height/2) - 40)
-     .attr("dx", "1em")
-     .style("text-anchor", "start")
-     .attr("fill", "#5D6971")
-     .text("Threshold");
+  d3.json("/retrieve_profit_curve", function(thisData) {
+    draw(thisData);
+  });
 
-  g.append("g")
-      .attr("class", "axis axis--y")
-      .call(d3.axisLeft(y))
-   .append("text")
-     .attr("class", "axis-title")
-     .attr("transform", "rotate(-90)")
-     .attr("y", -42)
-     .attr("dy", ".1em")
-     .attr("x", -height/2 + 30)
-     .attr("dx", ".1em")
-     .style("text-anchor", "end")
-     .attr("fill", "#5D6971")
-     .text("Cost ($)");
+  let draw = function(data) {
+    $("svg").empty()
+    var x = d3.scaleLinear()
+        .range([0, width]);
 
-  var line_stuff = g.selectAll(".line")
-      .data([data]);
+    var y = d3.scaleLinear()
+        .range([0, height]);
 
-  line_stuff.enter().append("path").classed("line", true)
-             .merge(line_stuff)
-             .attr("d", line)
-             .attr("fill", "none")
-             .attr("stroke", "black")
-             .attr("stroke-dasharray", function(d) {
-               return this.getTotalLength()
-             })
-             .attr("stroke-dashoffset", function(d) {
-               return this.getTotalLength()
-             });
+    var line = d3.line()
+        .x(d => x(d.threshold))
+        .y(d => y(d.loss));
 
-  g.append("path")
-      .datum(horiz)
-      .attr("class", "horiz_line")
-      .style("stroke-dasharray", ("3, 3"))
-      .attr("d", horiz_line);
+    var y_max = d3.max(data, d => d.loss);
+    var x_val = data.filter(d => d.loss === y_max)[0].threshold;
+    var horiz = [{"x1": 0.0, "y1": y_max}, {"x1": x_val, "y1": y_max}]
+    var vert = [{"x2": x_val, "y2": d3.min(data, d => d.loss)}, {"x2": x_val, "y2": y_max}]
 
-  g.append("path")
-      .datum(vert)
-      .attr("class", "vert_line")
-      .style("stroke-dasharray", ("3, 3"))
-      .attr("d", vert_line);
+    var horiz_line = d3.line()
+        .x(d => x(d.x1))
+        .y(d => y(d.y1));
 
-  g.append("text")
-      .attr("class", "max_thresh")
-      .attr("x", x(x_val))
-      .attr("dx", "2em")
-      .attr("y", y(y_max))
-      .style("text-anchor", "end")
-      .text(Math.round(x_val * 100) / 100);
+    var vert_line = d3.line()
+        .x(d => x(d.x2))
+        .y(d => y(d.y2));
+
+    var g = svg.append("g")
+        .attr("transform", "translate(" + (margin.left + 50) + "," + margin.top + ")");
+    
+    x.domain([0, d3.max(data, d => d.threshold)]);
+    y.domain([d3.max(data, d => d.loss), d3.min(data, d => d.loss)]);
+
+    g.append("g")
+        .attr("class", "axis axis--x")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x))
+      .append("text")
+       .attr("class", "axis-title")
+       .attr("y", 18)
+       .attr("dy", "1em")
+       .attr("x", (height/2) - 40)
+       .attr("dx", "1em")
+       .style("text-anchor", "start")
+       .attr("fill", "#5D6971")
+       .text("Threshold");
+
+    g.append("g")
+        .attr("class", "axis axis--y")
+        .call(d3.axisLeft(y))
+     .append("text")
+       .attr("class", "axis-title")
+       .attr("transform", "rotate(-90)")
+       .attr("y", -42)
+       .attr("dy", ".1em")
+       .attr("x", -height/2 + 30)
+       .attr("dx", ".1em")
+       .style("text-anchor", "end")
+       .attr("fill", "#5D6971")
+       .text("Cost ($)");
+
+    var line_stuff = g.selectAll(".line")
+        .data([data]);
+
+    line_stuff.enter().append("path").classed("line", true)
+               .merge(line_stuff)
+               .attr("d", line)
+               .attr("fill", "none")
+               .attr("stroke", "black")
+               .attr("stroke-dasharray", function(d) {
+                 return this.getTotalLength()
+               })
+               .attr("stroke-dashoffset", function(d) {
+                 return this.getTotalLength()
+               });
+
+    g.append("path")
+        .datum(horiz)
+        .attr("class", "horiz_line")
+        .style("stroke-dasharray", ("3, 3"))
+        .attr("d", horiz_line);
+
+    g.append("path")
+        .datum(vert)
+        .attr("class", "vert_line")
+        .style("stroke-dasharray", ("3, 3"))
+        .attr("d", vert_line);
+
+    g.append("text")
+        .attr("class", "max_thresh")
+        .attr("x", x(x_val))
+        .attr("dx", "2em")
+        .attr("y", y(y_max))
+        .style("text-anchor", "end")
+        .text(Math.round(x_val * 100) / 100);
 
 
-  g.selectAll(".line")
-    .transition()
-    .duration(1000)
-    .ease(d3.easeLinear)
-    .attr("stroke-dashoffset", 0);
+    g.selectAll(".line")
+      .transition()
+      .duration(1000)
+      .ease(d3.easeLinear)
+      .attr("stroke-dashoffset", 0);
 
-};
+  };
 
-$(document).ready(function() {
-  $("button#calculate").click(function() {
-    $(".assess-box").children("button").remove();
-    $("p#assessment").text("Loading...")
-    let metrics = get_metrics();
-    send_metrics(metrics);
+  $(document).ready(function() {
+    $("button#calculate").click(function() {
+      $(".assess-box").children("button").remove();
+      $("p#assessment").text("Loading...")
+      let metrics = get_metrics();
+      send_metrics(metrics);
+    })
   })
-})
 
-let send_metrics = function(metrics) {
-  $.ajax({
-    url: '/generate',
-    contentType: "application/json; charset=utf-8",
-    type: 'POST',
-    data: JSON.stringify(metrics),
-    success: function(data) {
-      draw(data);
-      statement(data);
-    }
-  });
-}
-
-let get_metrics = function() {
-  let revenue = $("input#revenue").val()
-  let maintenance = $("input#maintenance").val()
-  let repair = $("input#repair").val()
-  return {"user_input": [revenue, maintenance, repair]}
-};
-
-function statement(data) {
-  var threshold = $("text.max_thresh").text();
-  var threshold_statement = threshold + " is the optimal threshold for the given revenue, maintenance, and repair costs. Would you like to save this threshold?"
-  $("p#assessment").text(threshold_statement)
-  var cancel = $('<button type="button" class="btn btn-danger id=cancel">Cancel</button>').click(function() {
-    $(".assess-box").empty().append('<p id="assessment"></p>')
-  });
-  var save = $('<button type="button" class="btn btn-success id=save">Save</button>').click(function() {
+  let send_metrics = function(metrics) {
     $.ajax({
-      url: '/save_profit_curve',
+      url: '/generate',
       contentType: "application/json; charset=utf-8",
       type: 'POST',
-      data: JSON.stringify({"threshold": threshold,
-                            "data": data,
-                            "metrics": get_metrics()}),
-      success: function() {
-        location.reload();
+      data: JSON.stringify(metrics),
+      success: function(data) {
+        draw(data);
+        statement(data);
       }
-    })
-  });
-  $(".assess-box").append(cancel).append(save)
-}
+    });
+  }
+
+  let get_metrics = function() {
+    let revenue = $("input#revenue").val()
+    let maintenance = $("input#maintenance").val()
+    let repair = $("input#repair").val()
+    return {"user_input": [revenue, maintenance, repair]}
+  };
+
+  function statement(data) {
+    var threshold = $("text.max_thresh").text();
+    var threshold_statement = threshold + " is the optimal threshold for the given revenue, maintenance, and repair costs. Would you like to save this threshold?"
+    $("p#assessment").text(threshold_statement)
+    var cancel = $('<button type="button" class="btn btn-danger id=cancel">Cancel</button>').click(function() {
+      $(".assess-box").empty().append('<p id="assessment"></p>')
+    });
+    var save = $('<button type="button" class="btn btn-success id=save">Save</button>').click(function() {
+      $.ajax({
+        url: '/save_profit_curve',
+        contentType: "application/json; charset=utf-8",
+        type: 'POST',
+        data: JSON.stringify({"threshold": threshold,
+                              "data": data,
+                              "metrics": get_metrics()}),
+        success: function() {
+          location.reload();
+        }
+      })
+    });
+    $(".assess-box").append(cancel).append(save)
+  }
+})(window, document);
