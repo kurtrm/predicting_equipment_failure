4e4fa38dccbefbfa3ff4848f4084319444cb7e51
Kurt Maurer <kurtrm@gmail.com>
2018-05-17 16:41:27 -0700

    Got the scores to display.

---
+++ src/scripts/model_metrics.py
@@ -0,0 +1,32 @@
+"""
+Various functions that compute metrics on the model.
+"""
+from sklearn.metrics import roc_auc_score, precision_recall_fscore_support
+from sklearn.externals import joblib
+import numpy as np
+
+from scripts import db
+from scripts.profit_curve import threshold_prediction
+
+
+model = joblib.load('static/models/final_grad_boost.pkl')
+threshold = db.select_threshold()[0] / 100
+fetched = db.fetch_test_data()
+test_set = np.array(fetched)[:, 1:]
+X_test, y_test = test_set[:, :-1], test_set[:, -1]
+y_score = model.predict_proba(X_test)[:, 1]
+y_threshold_score = threshold_prediction(model, X_test, threshold)
+
+
+def get_auc_score():
+    """
+    Use sklearn roc_auc_score to compute auc.
+    """
+    return roc_auc_score(y_test, y_score)
+
+
+def precision_recall_f1():
+    """
+    Wraps sklearn's precision_recall_f1_support function.
+    """
+    return precision_recall_fscore_support(y_test, y_threshold_score, average='binary')
