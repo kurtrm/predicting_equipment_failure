c223a624d2d3fe60845166c6bc60381125c9453a
Kurt Maurer <kurtrm@gmail.com>
2018-05-17 11:43:21 -0700

    I think the unit analysis styling is in a good place.

--- src/static/js/new_transformer.js
+++ src/static/js/new_transformer.js
@@ -32,23 +32,30 @@
 
     let display_prediction = function(data) {
         $(".results").show()
-        $("span#probability").html(data.probability)
-        $("span#threshold").html(data.threshold)
+        $("span#probability").html(data.probability + "%")
+        $("span#threshold").html(data.threshold + "%")
         if (data.probability < data.threshold) {
+            $("span#probability").addClass("text-danger")
             var assess = "There is a " + data.probability + "% probability that this transformer will not incur additional costs." +
-                         " Additionally, based on the selected threshold of " + data.threshold + ", " + 
-                         "it is recommended that an enhanced preventive maintenance schedule be applied to this unit.";
+                         " Additionally, based on the selected threshold of " + data.threshold + "%, " + 
+                         "it is recommended that an <b>enhanced preventive maintenance schedule</b> be applied to this unit.";
         } else {
+            $("span#probability").addClass("text-success")
             var assess = "There is a " + data.probability + "% probability that this transformer will not incur additional costs." +
-                         " Additionally, based on the selected threshold of " + data.threshold + ", " + 
-                         "it is recommended that this unit maintain a normal preventive maintenance schedule.";
+                         " Additionally, based on the selected threshold of " + data.threshold + "%, " + 
+                         "it is recommended that this unit maintain a <b>normal preventive maintenance schedule</b>.";
         }
         $("span#assessment").html(assess)
     };
 
     $("button#send").click(function() {
-        console.log('Here');
         let inputs = get_form_inputs();
+        if (inputs["Age"] === "" || isNaN(inputs["Age"]) || inputs["Age"] < 0) {
+            $(".invalid-tooltip").show();
+            return
+        } else {
+            $(".invalid-tooltip").hide();
+        };
         send_form_inputs(inputs);
     });
 })(window, document);
