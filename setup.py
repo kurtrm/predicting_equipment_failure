2fc23e6314022e07b233f5b4ccf254356caf043d
Kurt Maurer <kurtrm@gmail.com>
2018-05-16 18:13:18 -0700

    Made requirements file. Removed unneeded dependencies. Updated setup.py

--- setup.py
+++ setup.py
@@ -6,12 +6,15 @@ extra_packages = {
     'testing': ['pytest']
 }
 
+description = '''Project leveraging machine learning to help determine if
+electrical transformers will require more intensive maintenance schedules.'''
 
 setup(
-    name='Predicting Equipment Failure',
-    description='Project aiming to predict equipment failure.',
+    name='predicting_equipment_failure',
+    description=description,
     version=0.0,
     author='Kurt Maurer',
     author_email='kurtrm@gmail.com',
+    install_requires=['numpy', 'pytz', 'pandas', 'sklearn', 'flask', 'psycopg2', 'scipy'],
     extras_require=extra_packages
 )
